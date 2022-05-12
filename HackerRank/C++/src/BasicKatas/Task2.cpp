563c9540d85dae7a482a4d39b131cae8c5af993d
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2022-05-12 21:01:30 +0200

    Added task02 in c++

---
+++ HackerRank/C++/src/BasicKatas/Task2.cpp
@@ -0,0 +1,15 @@
+/*
+    Write a C++ Program to Display Number (Entered by the User). Here’s simple Program to Print Number (Entered by the User) in C++ Programming Language.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    std::cout << "Enter an integer :: ";
+    std::cin >> number;
+    std::cout << "\nThe number is :: " << number << "\n";
+    return 0;
+}
